fc2bbfee8ba543fbca373fc6b657c8eda82fe24e
Sourabh <sk857087@gmail.com>
2025-06-16 15:39:44 +0530

    error handlers

--- src/app.js
+++ src/app.js
@@ -6,16 +6,19 @@ const PORT = 5000;
 
 app.use("/admin", adminAuth)
 
-app.get("/admin/allData", (req, res, next) =>{
-    res.send("All Data is Fetched")
+app.get("/admin/allData", (req,res,next) =>{
+        throw new Error("please contact support");
 });
 
-app.get("/user/userData",userAuth,(req,res)=>{
+app.get("/user/userData",userAuth,(err,req,res,next)=>{
     res.send(
         "user data is fetched"
     )
 })
 
+app.use("/", (err,req,res,next) => {
+    res.status(500).send({ error: err.message });
+})
 
 
 
